bb4daceba6dece3c94a002568c1d844800a2ef0c
seanyoon814 <seanyoonn814@gmail.com>
2022-06-28 22:50:57 -0700

    added login and schedule endpoints; used prof login page

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const { Pool } = require('pg');
+var pool;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express()
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('pages/index'))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.get('/login', (req, res)=>{
+  res.render('pages/login')
+})
+
+app.get('/schedule', (req, res)=>{
+  res.render('pages/schedule')
+})
+
