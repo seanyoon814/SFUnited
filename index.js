966eea7d1bf41f6ea86bed249696710411925d92
seanyoon814 <seanyoonn814@gmail.com>
2022-06-29 16:55:17 -0700

    added rmp web scraper. kind of slow O(N^2). req take ~1-2 secs

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require('express')
+const axios = require('axios')
+const cheerio = require('cheerio')
+const fs = require('fs')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
 var pool;
 
+var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
+async function scrape()
+{
+  console.log("Ran")
+  for(let i = 0; i<letters.length; i++)
+  {
+    const url = 'https://ratemyprof-api.vercel.app/api/getProf?first=bobby&last=chan&schoolCode=U2Nob29sLTE0Nj' + letters[i]
+    const { data } = await axios.get(url);
+    try{
+      if(data['ratings'][0]['class'].includes("CMPT"))
+      {
+        console.log(data['firstName'])
+        console.log(data['lastName'])
+        console.log(data['avgRating'])
+        {break;}
+      }
+    }
+    catch(err){
+
+    }
+  }
+}
 var app = express()
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
@@ -15,6 +40,7 @@ app.get('/', (req, res) => res.render('pages/index'))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 app.get('/login', (req, res)=>{
+  scrape();
   res.render('pages/login')
 })
 
